68131f3aba20c3370b89b919a8fc36748047a3c0
raysakarina <raysa.diaz@hotmail.com>
2021-10-18 11:15:10 -0500

    este intento no funciono, iniciamos de nuevo...

--- proyectosclases/tienda2/.cache/sync-requires.js
+++
@@ -1,9 +0,0 @@
-
-// prefer default export if available
-const preferDefault = m => (m && m.default) || m
-
-
-exports.components = {
-  "component---cache-dev-404-page-js": preferDefault(require("/home/git/raysakarina.github.io/proyectosclases/tienda2/.cache/dev-404-page.js"))
-}
-
--- proyectosclases/tienda2/scr/pages/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function index() {
-    return (
-        <div>
-            <h1>Tienda 2</h1>
-        </div>
-    )
-}
